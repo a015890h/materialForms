c662917494027af5df9516812313223bed29e6d6
Mohammed Amin <a015890h@student.staffs.ac.uk>
2018-03-07 12:27:26 +0000

    Php
    Added Php to website for database connection

---
+++ sendmsg.php
@@ -0,0 +1,27 @@
+<?php
+$servername = "localhost:3306";
+$username = "Amin123";
+$password = "Amin123";
+$dbname = "materialDb";
+
+$firstName = $_POST['firstName'];
+$lastName = $_POST['lastName'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error){
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "INSET INTO contactUs (firstName, lastName, email, message) VALUES ('$firstName', '$lastName', '$email', '$message')";
+
+if ($conn->query($sql) === TRUE){
+    echo "New record created successfuly";
+} else{
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
